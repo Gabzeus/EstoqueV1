e124504b3f58fb069bc57d742714a32695d81139
GABRIEL NATHAN STIEHLER DA SILVA <gabriel.silva28@alunos.sc.senac.br>
2021-02-17 01:24:49 -0300

    Att notificações

--- EstoqueV1/Notificações.cs
+++ EstoqueV1/Notificações.cs
@@ -16,5 +16,15 @@ namespace EstoqueV1
         {
             InitializeComponent();
         }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
